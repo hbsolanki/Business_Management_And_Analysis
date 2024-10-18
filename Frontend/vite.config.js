a8bd9ed8400c23a372d161e3c132ffed022da793
Hardik Solanki <136411045+hbsolanki@users.noreply.github.com>
2024-10-18 22:55:59 +0530

    Update vite.config.js

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -3,10 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    proxy: {
-      "/API": "https://business-management-and-analysis.onrender.com/",
-    },
-  },
+  
   plugins: [react()],
 });

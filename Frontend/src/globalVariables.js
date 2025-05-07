3da8962491fea771ffb762b86eef8435c2fed3e7
Solanki Hardik <sh369915@gmail.com>
2025-05-08 00:04:37 +0530

    some changes

--- Frontend/src/globalVariables.js
+++ Frontend/src/globalVariables.js
@@ -1,5 +1,5 @@
-let API = "http://127.0.0.1:8000";
-// let API = "https://business-management-and-analysis-backend.onrender.com";
+// let API = "http://127.0.0.1:8000";
+let API = "https://business-management-and-analysis-backend.onrender.com";
 
 export const setGlobalVariable = (value) => {
   API = value;
